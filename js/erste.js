3c7396461344a5d7c0805eb77d4a8449025fa502
Relysia <baranyairiki@gmail.com>
2020-08-02 21:21:56 +0200

    Changes made pro

---
+++ js/erste.js
@@ -0,0 +1,27 @@
+function calcAmount()
+{
+    let percent = 100;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountNumber = parseFloat(amountInput.value);
+    amountNumber = (isNaN(amountNumber)) ? 0 : amountNumber;
+    
+    let trxFee = document.querySelector("input[name='trx-fee']");
+    let trxFeeValue = parseFloat(trxFee.value);
+
+    let additionalPrice = document.querySelector("input[name='additional-price']");
+    let additionalPriceValue = parseFloat(additionalPrice.value);
+
+    let optionOne = document.querySelector("input[name='option-one']");
+    let optionOneValue = parseFloat(optionOne.value);
+
+    showSumPrice(amountNumber, trxFeeValue, percent, optionOneValue, additionalPriceValue);
+}
+
+
+function showSumPrice(amountNumber, trxFeeValue, percent, optionOneValue, additionalPriceValue)
+{
+        let showAmount = document.querySelector("span.show-amount");
+
+        let amount = (amountNumber / percent * trxFeeValue) + additionalPriceValue + (amountNumber / percent * optionOneValue);
+        showAmount.innerHTML = amount;
+}
