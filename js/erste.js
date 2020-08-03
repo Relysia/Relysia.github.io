a82d1eb4ea52695eec3b4ea3668ff56589117ab6
Relysia <baranyairiki@gmail.com>
2020-08-03 20:19:53 +0200

    Bank Calculator 1.0

--- js/erste.js
+++ js/erste.js
@@ -8,25 +8,40 @@ function calcAmount()
     let trxFee = document.querySelector("input[name='trx-fee']");
     let trxFeeValue = parseFloat(trxFee.value);
 
+    let trxMin = document.querySelector("input[name='trx-min']");
+    let trxMinValue = parseFloat(trxMin.value);
+
+    let trxMax = document.querySelector("input[name='trx-max']");
+    let trxMaxValue = parseFloat(trxMax.value);
+
     let additionalPrice = document.querySelector("input[name='additional-price']");
     let additionalPriceValue = parseFloat(additionalPrice.value);
 
     let optionOne = document.querySelector("input[name='option-one']");
     let optionOneValue = parseFloat(optionOne.value);
 
-    showSumPrice(amountNumber, trxFeeValue, percent, optionOneValue, additionalPriceValue);
+    showSumPrice(amountNumber, trxFeeValue, trxMinValue, trxMaxValue, percent, optionOneValue, additionalPriceValue);
 }
 
 
-function showSumPrice(amountNumber, trxFeeValue, percent, optionOneValue, additionalPriceValue)
+function showSumPrice(amountNumber, trxFeeValue, trxMinValue, trxMaxValue, percent, optionOneValue, additionalPriceValue)
 {
-        let showAmount = document.querySelector("span.show-amount");
+    let showAmount = document.querySelector("span.show-amount");
 
-        let amount = (amountNumber / percent * trxFeeValue) + additionalPriceValue;
-        let trxAmount =  (amountNumber / percent * optionOneValue);
-        if (trxAmount >= 6000) 
-        {
-            trxAmount = 6000;
-        }
-        showAmount.innerHTML = amount + trxAmount;
+    let amount = (amountNumber / percent * trxFeeValue);
+    if (amount <= trxMinValue)
+    {
+        amount = trxMinValue;
+    }
+    else if (amount >= trxMaxValue)
+    {
+        amount = trxMaxValue;
+    }
+    let additionalPriceAmount = additionalPriceValue
+    let trxAmount =  (amountNumber / percent * optionOneValue);
+    if (trxAmount >= 6000) 
+    {
+        trxAmount = 6000;
+    }
+    showAmount.innerHTML = amount + additionalPriceAmount + trxAmount;
 }
