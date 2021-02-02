49354e2d6a0a61ff9a4c463f66f4b8fe4eafc2d3
Relysia <baranyairiki@gmail.com>
2021-02-02 13:14:37 +0100

    first react publish

--- javascript/main.js
+++ javascript/main.js
@@ -1,124 +1,184 @@
 // ELEMENT VARIABLES
 // GRID CONTENT
-let htmlLink = document.querySelector('#html')
-let cssLink = document.querySelector('#css')
-let sassLink = document.querySelector('#sass')
-let jsLink = document.querySelector('#js')
+let topLeft = document.querySelector("#html");
+let topRight = document.querySelector("#css");
+let bottomLeft = document.querySelector("#sass");
+let bottomRight = document.querySelector("#js");
 
 // LEFT AND RIGHT ARROW
-let leftArrow = document.querySelector('#left-icon')
-let rightArrow = document.querySelector('#right-icon')
-leftArrow.classList.add('hide')
+let leftArrow = document.querySelector("#left-icon");
+let rightArrow = document.querySelector("#right-icon");
+leftArrow.classList.add("hide");
 
 // BASIC FUNCTIONS
 // CHANGE AN ELEMENT CONTENT
 function changeContent(element, title, link, idName) {
-  element.textContent = title
+  element.textContent = title;
   setTimeout(function () {
-    element.setAttribute('href', link)
-  }, 100)
-  element.setAttribute('id', idName)
+    element.setAttribute("href", link);
+  }, 100);
+  element.setAttribute("id", idName);
 }
 // HIDE ONE ELEMENT
 function hideElement(element, className) {
-  element.classList.add(className)
+  element.classList.add(className);
 }
 // SHOW ALL ELEMENT
 
 // REMOVE HIDE FROM ONE ELEMENT
 function removeHide(element, className) {
-  element.classList.remove(className)
+  element.classList.remove(className);
 }
 // REMOVE HIDE FROM EVERY ELEMENT
 function removeAll(element1, element2, element3, element4, className) {
-  removeHide(element1, className)
-  removeHide(element2, className)
-  removeHide(element3, className)
-  removeHide(element4, className)
+  removeHide(element1, className);
+  removeHide(element2, className);
+  removeHide(element3, className);
+  removeHide(element4, className);
 }
 
 //FIRST PAGE LAYOUT
 function firstPage() {
-  changeContent(htmlLink, 'HTML', '#', 'html')
-  changeContent(cssLink, 'CSS', '#', 'css')
-  changeContent(sassLink, 'SASS', '#', 'sass')
-  changeContent(jsLink, 'JAVASCRIPT', '#', 'js')
-  changeContent(illustratorLink, 'ILLUSTRATOR', '#', 'illustrator')
-  removeAll(htmlLink, cssLink, sassLink, jsLink, 'hide-content')
-  hideElement(illustratorLink, 'hide-content')
-  leftArrow.className = "fas fa-home"
-  hideElement(leftArrow, 'hide')
-  removeHide(rightArrow, 'hide')
+  changeContent(topLeft, "HTML", "#", "html");
+  changeContent(topRight, "CSS", "#", "css");
+  changeContent(bottomLeft, "SASS", "#", "sass");
+  changeContent(bottomRight, "JAVASCRIPT", "#", "js");
+  changeContent(illustratorLink, "ILLUSTRATOR", "#", "illustrator");
+  removeAll(topLeft, topRight, bottomLeft, bottomRight, "hide-content");
+  hideElement(illustratorLink, "hide-content");
+  leftArrow.className = "fas fa-home";
+  hideElement(leftArrow, "hide");
+  removeHide(rightArrow, "hide");
 }
-leftArrow.addEventListener('click', firstPage)
+leftArrow.addEventListener("click", firstPage);
 
 // HTML SWITCH LAYOUT
 function htmlSwitch() {
-  changeContent(htmlLink, 'Plant a Tree', 'html/tree/plantatree.html', 'plant-a-tree')
-  changeContent(cssLink, 'Grandmas Recipes', 'html/recipe/index.html', 'grandmas-recipes')
-  hideElement(sassLink, 'hide-content')
-  hideElement(jsLink, 'hide-content')
-  removeHide(leftArrow, 'hide')
-  hideElement(rightArrow, 'hide')
+  changeContent(
+    topLeft,
+    "Plant a Tree",
+    "react/findmylist/index.html",
+    "plant-a-tree"
+  );
+  changeContent(
+    topRight,
+    "Grandmas Recipes",
+    "html/recipe/index.html",
+    "grandmas-recipes"
+  );
+  hideElement(bottomLeft, "hide-content");
+  hideElement(bottomRight, "hide-content");
+  removeHide(leftArrow, "hide");
+  hideElement(rightArrow, "hide");
 }
-htmlLink.addEventListener('click', htmlSwitch)
+topLeft.addEventListener("click", htmlSwitch);
 
 // CSS SWITCH LAYOUT
 function cssSwitch() {
-  changeContent(htmlLink, 'CSS Layouts', 'css/layouts/index.html', 'css-layouts')
-  changeContent(cssLink, 'Super Center Title', 'css/center/index.html', 'center-title')
-  changeContent(sassLink, 'Blog Page', 'css/catblog/home.html', 'blog-page')
-  changeContent(jsLink, 'Responsive Rainbow', 'css/rainbow/index.html', 'rainbow')
-  removeHide(leftArrow, 'hide')
-  hideElement(rightArrow, 'hide')
+  changeContent(
+    topLeft,
+    "CSS Layouts",
+    "css/layouts/index.html",
+    "css-layouts"
+  );
+  changeContent(
+    topRight,
+    "Super Center Title",
+    "css/center/index.html",
+    "center-title"
+  );
+  changeContent(bottomLeft, "Blog Page", "css/catblog/home.html", "blog-page");
+  changeContent(
+    bottomRight,
+    "Responsive Rainbow",
+    "css/rainbow/index.html",
+    "rainbow"
+  );
+  removeHide(leftArrow, "hide");
+  hideElement(rightArrow, "hide");
 }
-cssLink.addEventListener('click', cssSwitch)
+topRight.addEventListener("click", cssSwitch);
 
 // SASS SWITCH
 function sassSwitch() {
-  changeContent(htmlLink, 'SAS Space', 'sass/sas/index.html', 'sas-space')
-  changeContent(cssLink, 'Landing for a Cause', 'sass/landing/index.html', 'landing')
-  changeContent(sassLink, 'Travel Agency', 'sass/travel/index.html', 'travel-agency')
-  hideElement(jsLink, 'hide-content')
-  removeHide(leftArrow, 'hide')
-  hideElement(rightArrow, 'hide')
+  changeContent(topLeft, "SAS Space", "sass/sas/index.html", "sas-space");
+  changeContent(
+    topRight,
+    "Landing for a Cause",
+    "sass/landing/index.html",
+    "landing"
+  );
+  changeContent(
+    bottomLeft,
+    "Travel Agency",
+    "sass/travel/index.html",
+    "travel-agency"
+  );
+  hideElement(bottomRight, "hide-content");
+  removeHide(leftArrow, "hide");
+  hideElement(rightArrow, "hide");
 }
-sassLink.addEventListener('click', sassSwitch)
+bottomLeft.addEventListener("click", sassSwitch);
 
 // JAVASCRIPT SWITCH
 function jsSwitch() {
-  changeContent(htmlLink, 'Multi Color Buttons', 'js/buttons/index.html', 'buttons')
-  changeContent(cssLink, 'Create Your Character', 'js/character/index.html', 'character')
-  changeContent(sassLink, 'Book a Table', 'js/booking/index.html', 'booking')
-  hideElement(jsLink, 'hide-content')
-  removeHide(leftArrow, 'hide')
-  hideElement(rightArrow, 'hide')
+  changeContent(
+    topLeft,
+    "Multi Color Buttons",
+    "js/buttons/index.html",
+    "buttons"
+  );
+  changeContent(
+    topRight,
+    "Create Your Character",
+    "js/character/index.html",
+    "character"
+  );
+  changeContent(bottomLeft, "Book a Table", "js/booking/index.html", "booking");
+  hideElement(bottomRight, "hide-content");
+  removeHide(leftArrow, "hide");
+  hideElement(rightArrow, "hide");
 }
-jsLink.addEventListener('click', jsSwitch)
+bottomRight.addEventListener("click", jsSwitch);
 
 // SECOND PAGE LAYOUT
-let illustratorLink = document.querySelector('#illustrator')
+let illustratorLink = document.querySelector("#illustrator");
 
 function secondPage() {
-  hideElement(htmlLink, 'hide-content')
-  hideElement(cssLink, 'hide-content')
-  hideElement(sassLink, 'hide-content')
-  hideElement(jsLink, 'hide-content')
-  leftArrow.className = "fas fa-chevron-circle-left"
-  hideElement(rightArrow, 'hide')
-  removeHide(illustratorLink, 'hide-content')
+  hideElement(topLeft, "hide-content");
+  hideElement(topRight, "hide-content");
+  hideElement(bottomLeft, "hide-content");
+  hideElement(bottomRight, "hide-content");
+  leftArrow.className = "fas fa-chevron-circle-left";
+  hideElement(rightArrow, "hide");
+  removeHide(illustratorLink, "hide-content");
 }
-rightArrow.addEventListener('click', secondPage)
+rightArrow.addEventListener("click", secondPage);
 
 // ILLUSTRATOR SWITCH
 function illustratorSwitch() {
-  removeAll(htmlLink, cssLink, sassLink, jsLink, 'hide-content')
-  hideElement(htmlLink, 'hide-content')
-  changeContent(illustratorLink, 'Fancy Fonts', 'illustrator/fancy/index.html', 'fancy')
-  changeContent(cssLink, 'Simple Shape', 'illustrator/shape/index.html', 'shape')
-  changeContent(sassLink, 'Interactive Logo', 'illustrator/logo/logo.html', 'logo')
-  hideElement(jsLink, 'hide-content')
-  leftArrow.className = "fas fa-home"
+  removeAll(topLeft, topRight, bottomLeft, bottomRight, "hide-content");
+  hideElement(topLeft, "hide-content");
+  changeContent(
+    illustratorLink,
+    "Fancy Fonts",
+    "illustrator/fancy/index.html",
+    "fancy"
+  );
+  changeContent(
+    topRight,
+    "Simple Shape",
+    "illustrator/shape/index.html",
+    "shape"
+  );
+  changeContent(
+    bottomLeft,
+    "Interactive Logo",
+    "illustrator/logo/logo.html",
+    "logo"
+  );
+  hideElement(bottomRight, "hide-content");
+  leftArrow.className = "fas fa-home";
 }
 
-illustratorLink.addEventListener('click', illustratorSwitch)
+illustratorLink.addEventListener("click", illustratorSwitch);
